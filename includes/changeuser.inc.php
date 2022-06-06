7a7a439952bbb167f028a4072e13b404826f3033
Mira Stejskal <65676574+swajp@users.noreply.github.com>
2022-06-06 13:58:53 +0200

    Update changeuser.inc.php
    removed userid

--- includes/changeuser.inc.php
+++ includes/changeuser.inc.php
@@ -3,12 +3,11 @@ if (isset($_POST["submit"])) {
 
     $email = $_POST["email"];
     $username = $_POST["username"];
-    $userid = $_POST["id"];
 
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
 
-    changeUser($conn, $email, $username, $userid);
+    changeUser($conn, $email, $username);
 }
 
 else {
